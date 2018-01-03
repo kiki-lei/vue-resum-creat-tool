df02c62f9f86a7126e15133b0654b5bb7fe7dade
kiki-lei <358613224@qq.com>
2018-01-03 17:28:04 +0800

    test

---
+++ src/components/HeaderImg.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header-img">
+    <img :src="pictureUrl" alt="头像">
+    <input type="file" class="choose-img" @change="onFileChange">
+  </div>
+</template>
+
+<style>
+.header-img {
+  margin-bottom: 30px;
+}
+.header-img,
+.header-img img {
+  position: relative;
+  width: 218px;
+  height: 218px;
+  box-shadow: 0 0 10px #ffffff;
+  border-radius: 5px;
+}
+.header-img .choose-img {
+  width: 218px;
+  height: 218px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  opacity: 0;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [],
+      pictureUrl: "../../static/img/headerImg.jpg"
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+
+      var _this = this;
+      var reader = null;
+      // 浏览器是否兼容FileReader
+      if(window.FileReader){
+        reader = new window.FileReader();
+        reader.readAsDataURL(files[0]);
+        reader.onload = function(e) {
+          _this.pictureUrl = e.target.result;
+        };
+      }
+    }
+  }
+};
+</script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="skills">
+    <!-- 标题 -->
+    <div class="titleBox">
+      <span class="title">技能特点</span>
+      <a href="javascrpt:void(0);" class="add addBtn" @click="addSkills">
+        <mu-icon value="add" color="#ffffff" />
+      </a>
+    </div>
+
+    <!-- 添加弹窗 -->
+    <div class="popBox">      
+      <mu-dialog :open="dialog" title="添加技能点" @close="close">
+        <h4>已经掌握的技术</h4>
+        <mu-text-field hintText="请输入名称..." type="type" fullWidth />
+        <mu-slider v-model="value1" class="skill-exp" :step="10" :min="0" :max="100" />{{value1}}
+
+        <mu-flat-button slot="actions" @click="close" primary label="取消"/>
+        <mu-flat-button slot="actions" primary @click="close" label="确定"/>
+      </mu-dialog>
+    </div>
+  </div>
+</template>
+
+<style>
+.titleBox {
+  margin-top: 30px;
+  border-bottom: 1px solid #ffffff;
+  padding-bottom: 10px;
+}
+.title {
+  font-size: 16px;
+}
+.addBtn {
+  float: right;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+      value1: 100
+    }
+  },
+  methods: {
+    addSkills () {
+      this.dialog = true
+    },
+    close () {
+      this.dialog = false
+    }
+  }
+}
+</script>
